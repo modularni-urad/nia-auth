e944b245eb50cb6847dc90149696866677f4daa9
Vaclav Klecanda <vencax@vxk.cz>
2021-08-14 21:55:28 +0200

    initial

---
+++ api/routes.js
@@ -0,0 +1,56 @@
+
+export default function (app, express) {
+  app.use(express.urlencoded({ extended: true }))
+
+  app.get('/login', _loadConfig, function (req, res, next) {
+    const opts = {
+      attrs: [
+        { name: NIA.PROFILEATTRS.PERSON_IDENTIFIER, required: true },
+        { name: NIA.PROFILEATTRS.GIVEN_NAME, required: true },
+        { name: NIA.PROFILEATTRS.FAMILY_NAME, required: false },
+        { name: NIA.PROFILEATTRS.CURRENT_ADDRESS, required: true },
+        { name: NIA.PROFILEATTRS.CZMORIS_TR_ADRESA_ID, required: true },
+        { name: NIA.PROFILEATTRS.DATE_OF_BIRTH, required: true },
+        { name: NIA.PROFILEATTRS.EMAIL, required: false },
+        { name: NIA.PROFILEATTRS.CZMORIS_PHONE_NUMBER, required: true },
+        { name: NIA.PROFILEATTRS.CZMORIS_ID_TYPE, required: true },
+        { name: NIA.PROFILEATTRS.CZMORIS_ID_NUMBER, required: true }
+      ],
+      level: NIA.LOA.SUBSTANTIAL
+    }
+
+    req.NIAConnector.createAuthRequestUrl(opts).then(loginUrl => {
+      res.redirect(loginUrl)
+    }).catch(next)
+  })
+
+  app.post('/ExternalLogin', _loadConfig, function (req, res, next) {
+    req.NIAConnector.postAssert(req.body).then(samlResponse => {
+      req.session.user = samlResponse
+      res.redirect(process.env.AFTERLOGIN_URL)
+    }).catch(next)
+  })
+
+  app.get('/logout', _loadConfig, (req, res, next) => {
+    const nameId = req.user.NameID
+    const sessionIndex = req.user.SessionIndex
+    req.NIAConnector.createLogoutRequestUrl(nameId, sessionIndex)
+      .then(logoutUrl => { res.redirect(logoutUrl) })
+      .catch(next)
+  })
+
+  app.post('/ExternalLogout', _loadConfig, (req, res, next) => {
+    try {
+      const samlResponse = req.NIAConnector.logoutAssert(req.body)
+      res.json(samlResponse)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  function _loadConfig (req, res, next) {
+    const domain = process.env.DOMAIN || req.hostname
+    req.orgid = D2O[domain]
+    return req.orgid ? next() : next(404)
+  }
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import express from 'express'
+
+import initRoutes from './api/routes'
+
+export default async function init (mocks = null) {
+  const app = express()
+  app.use(morgan('dev'))
+
+  initRoutes(app, express)
+
+  app.use((err, req, res, next) {
+    const status = err.status ||
+      isNaN(Number(err.message)) ? 400 : Number(err.message)
+    res.status(status).send(err.message || err)
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('---------------------------------------------------------')
+      console.log(err)
+      console.log('---------------------------------------------------------')
+    }
+  })
+
+  return app
+}
